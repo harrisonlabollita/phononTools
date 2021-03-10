6540c3b734ba7d444c7ee16ebacf5e9ee0f01a89
harrisonlabollita <hlabollita0219@lions.piedmont.edu>
2021-03-09 19:39:28 -0700

    working frozen script

--- src/frozen.py
+++ src/frozen.py
@@ -20,12 +20,12 @@ for i in range(1, maxDisplacement + 1):
     if i < 10:
         os.mkdir("disp-00{}".format(i))
         os.rename("POSCAR-00{}".format(i), "disp-00{}/POSCAR".format(i))
-        os.system("cp INCAR POTCAR KPOINTS disp-00{}".format(i))
+        os.system("cp INCAR POTCAR KPOINTS {} disp-00{}".format(slurm, i))
     if i >= 10 and i < 100:
         os.mkdir("disp-0{}".format(i))
         os.rename("POSCAR-0{}".format(i), "disp-0{}/POSCAR".format(i))
-        os.system("cp INCAR POTCAR KPOINTS disp-0{}".format(i))
+        os.system("cp INCAR POTCAR KPOINTS {} disp-0{}".format(slurm, i))
     if i >= 100:
         os.mkdir("disp-{}".format(i))
         os.rename("POSCAR-{}".format(i), "disp-{}/POSCAR".format(i))
-        os.system("cp INCAR POTCAR KPOINTS disp-{}".format(i))
+        os.system("cp INCAR POTCAR KPOINTS {} disp-{}".format(slurm, i))
